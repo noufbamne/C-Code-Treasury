505f0c984f60762ed6ccb359e20fcc9a918f1a82
Nouf Bamne <noufbamne@gmail.com>
2026-02-20 00:15:58 +0530

    Add files via upload

---
+++ declaration.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    printf("Addition is %d\n",12+11);
+    printf("Subtraction is %d\n",12-11);
+    printf("Division is %f\n",12/11);
+    printf("Multiplication is %d\n",12*11);
+
+    return 0;
+}
